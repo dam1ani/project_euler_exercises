f9cffc50418dd3dec1404e9a854993f0cae48424
dam1ani <damianpaluca@outlook.com>
2023-09-29 20:29:43 +0200

    Remove comment

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     problems_1_50::problem_2();
     problems_1_50::problem_3();
     problems_1_50::problem_4();
-    //problems_1_50::problem_5();
+    problems_1_50::problem_5();
     problems_1_50::problem_6();
     problems_1_50::problem_7();
     problems_1_50::problem_8();
